2f1457553e37582e23d4f8ed8e91262c9a174f83
BigtoC <taitocdt@gmail.com>
2020-03-05 12:57:02 +0800

    Adjust 3v3

--- assets/scripts/timedown/threeVthree.js
+++ assets/scripts/timedown/threeVthree.js
@@ -2,83 +2,81 @@
 
 
 define(function(require,exports,module){
-     var $ = require('jquery');//引入jquery
-     require('onepage')($);
+    var $ = require('jquery');//引入jquery
+    require('onepage')($);
      // require('boot')($);//共享给jquery
-     require('timer')($);//共享给jquery
-     require('jplay')($);//共享给jquery
-     require('jstorage')($);//共享给jquery
+    require('timer')($);//共享给jquery
+    require('jplay')($);//共享给jquery
+    require('jstorage')($);//共享给jquery
      // require('transit')($);//共享给jquery
      // require('ferroSilder')($);//共享给jquery
      // 
-     $("#jquery_jplayer_1").jPlayer({ready: function () {  //30秒铃声
-         $(this).jPlayer("setMedia", {
+    $("#jquery_jplayer_1").jPlayer({ready: function () {  //30秒铃声
+        $(this).jPlayer("setMedia", {
             volume: 100,
-          wav:"assets/res/l30s.mp3"
-         });
+            wav:"assets/res/l30s.mp3"
+        });
         },
         swfPath: "assets/res/",
         supplied: "wav,mp3"
-     });
+    });
 
-     $("#jquery_jplayer_2").jPlayer({ready: function () {  //时间到铃声
-         $(this).jPlayer("setMedia", {
-          volume :100,
-          wav:"assets/res/l1s.mp3"
-         });
+    $("#jquery_jplayer_2").jPlayer({ready: function () {  //时间到铃声
+        $(this).jPlayer("setMedia", {
+            volume :100,
+            wav:"assets/res/l1s.mp3"
+        });
         },
         swfPath: "assets/res/",
         supplied: "wav,mp3"
-     });
+    });
 
-     $("#jquery_jplayer_3").jPlayer({ready: function () {
-         $(this).jPlayer("setMedia", {
+    $("#jquery_jplayer_3").jPlayer({ready: function () {
+        $(this).jPlayer("setMedia", {
             volume: 100,
-          wav:"assets/res/ht.mp3"
-         });
+            wav:"assets/res/ht.mp3"
+        });
         },
         swfPath: "assets/res/",
         supplied: "wav,mp3"
-     });
+    });
 
     $("#play1").click(function(){
         $("#jquery_jplayer_1").jPlayer( "play" );
-     });
-     $("#play2").click(function(){
+    });
+    $("#play2").click(function(){
         $("#jquery_jplayer_2").jPlayer( "play" );
-     });
+    });
 
-     $("#play3").click(function(){
+    $("#play3").click(function(){
         $("#jquery_jplayer_3").jPlayer( "play" );
-     });
-     $("#play4").click(function(){
+    });
+    $("#play4").click(function(){
         $("#jquery_jplayer_3").jPlayer( "play" );
-     });
-     $("#btn1").click(function(){
+    });
+    $("#btn1").click(function(){
         $("#play1").click();
-     });
-     $("#btn2").click(function(){
+    });
+    $("#btn2").click(function(){
         $("#play2").click();
-     });
-     $("#btn3").click(function(){
+    });
+    $("#btn3").click(function(){
         $("#play3").click();
-     });
-     $("#btn4").click(function(){
+    });
+    $("#btn4").click(function(){
         $("#play4").click();
-     });
-
-     $('.main').onepage_scroll({
+    });
+    $('.main').onepage_scroll({
         sectionContainer: ".container-fluid",
         easing: "ease-in",
         loop: false
         // direction: "horizontal"
-     });
-
+    });
 
     //以下是计时时间
 
     $("#timedown1").custimer({  //正一陈词
-        time_length:210,
+        time_length:240,
         begin_btn_id:"beginBtn1",
         stop_btn_id:"stopBtn1",
         reset_btn_id: 'resetBtn1',
@@ -86,8 +84,8 @@ define(function(require,exports,module){
         time_ding_dang: [30,0]
     });
 
-    $("#timedown1_1").custimer({  //反一质正一
-        time_length:90,
+    $("#timedown1_1").custimer({  //反二质正一
+        time_length:180,
         begin_btn_id:"beginBtn1_1",
         stop_btn_id:"stopBtn1_1",
         reset_btn_id: 'resetBtn1_1',
@@ -145,7 +143,7 @@ define(function(require,exports,module){
     
 
     $("#timedown2").custimer({  //反一陈词
-        time_length:210,
+        time_length:240,
         begin_btn_id:"beginBtn2",
         stop_btn_id:"stopBtn2",
         reset_btn_id: 'resetBtn2',
@@ -153,8 +151,8 @@ define(function(require,exports,module){
         time_ding_dang: [30,0]
     });
 
-    $("#timedown2_1").custimer({  //正一质反一
-        time_length:90,
+    $("#timedown2_1").custimer({  //正三质反一
+        time_length:180,
         begin_btn_id:"beginBtn2_1",
         stop_btn_id:"stopBtn2_1",
         reset_btn_id: 'resetBtn2_1',
@@ -179,16 +177,15 @@ define(function(require,exports,module){
         time_ding_dang: [0]
     });
 
- 
 
     var flag5=2;
     $("#twbeginBtn4").on('click',function()
     {
-            flag5 =1;
+        flag5 =1;
     });
     $("#hdbeginBtn4").on('click',function()
     {
-            flag5 =0;
+        flag5 =0;
     });
     $("#flag5Btn").on('click',function(){
         $("#twresetBtn4").click();
@@ -211,8 +208,8 @@ define(function(require,exports,module){
         flag5=2;
     });
 
-    $("#timedown5").custimer({  //正二质询
-        time_length:180,
+    $("#timedown5").custimer({  //正二申论
+        time_length:240,
         begin_btn_id:"beginBtn5",
         stop_btn_id:"stopBtn5",
         reset_btn_id: 'resetBtn5',
@@ -220,7 +217,7 @@ define(function(require,exports,module){
         time_ding_dang: [30,0]
     });
 
-    $("#timedown5_1").custimer({  //反二质询
+    $("#timedown5_1").custimer({  //反三质正二
         time_length:180,
         begin_btn_id:"beginBtn5_1",
         stop_btn_id:"stopBtn5_1",
@@ -229,8 +226,8 @@ define(function(require,exports,module){
         time_ding_dang: [30,0]
     });
 
-    $("#timedown5_3").custimer({  //正三小结
-        time_length:90,
+    $("#timedown5_3").custimer({  //反二申论
+        time_length:240,
         begin_btn_id:"beginBtn5_3",
         stop_btn_id:"stopBtn5_3",
         reset_btn_id: 'resetBtn5_3',
@@ -238,8 +235,8 @@ define(function(require,exports,module){
         time_ding_dang: [30,0]
     });
 
-    $("#timedown5_4").custimer({  //反三小结
-        time_length:90,
+    $("#timedown5_4").custimer({  //正一质反二
+        time_length:180,
         begin_btn_id:"beginBtn5_4",
         stop_btn_id:"stopBtn5_4",
         reset_btn_id: 'resetBtn5_4',
@@ -247,36 +244,7 @@ define(function(require,exports,module){
         time_ding_dang: [30,0]
     });
 
-
-    //自由辩计时，Btn6正方先开始，Btn6_1反方
-    var flag6=2;
-    $("#beginBtn6").on('click',function()
-    {
-            flag6=1;
-    });
-    $("#beginBtn6_1").on('click',function()
-    {
-            flag6=0;
-    });
-    $("#doubleBtn").on('click',function(){
-        if(flag6 == 1){
-            $("#stopBtn6").click();
-            $("#beginBtn6_1").click();
-        }else if(flag6 == 0){
-            $("#stopBtn6_1").click();
-            $("#beginBtn6").click();
-        }else{  //flag6=2
-            $("#beginBtn6_1").click();  //这一行决定哪方先开始，在这里Btn6_1反方先开始
-        }
-    });
-
-    $("#doubleStopBtn").on('click',function(){
-        $("#stopBtn6").click();
-        $("#stopBtn6_1").click();
-        flag6=2;
-    });
-
-    $("#timedown6").custimer({
+    $("#timedown6").custimer({  //正三申论
         time_length:240,
         begin_btn_id:"beginBtn6",
         stop_btn_id:"stopBtn6",
@@ -285,8 +253,8 @@ define(function(require,exports,module){
         time_ding_dang: [30,0]
     });
 
-    $("#timedown6_1").custimer({
-        time_length:240,
+    $("#timedown6_1").custimer({  //反一质正三
+        time_length:180,
         begin_btn_id:"beginBtn6_1",
         stop_btn_id:"stopBtn6_1",
         reset_btn_id: 'resetBtn6_1',
@@ -294,8 +262,9 @@ define(function(require,exports,module){
         time_ding_dang: [30,0]
     });
 
-    $("#timedown7").custimer({  //结辩
-        time_length:210,
+
+    $("#timedown7").custimer({  //反三申论
+        time_length:240,
         begin_btn_id:"beginBtn7",
         stop_btn_id:"stopBtn7",
         reset_btn_id: 'resetBtn7',
@@ -303,19 +272,37 @@ define(function(require,exports,module){
         time_ding_dang: [30,0]
     });
 
-    $("#timedown7_1").custimer({  //结辩
-        time_length:210,
+    $("#timedown7_1").custimer({  //正二质反三
+        time_length:240,
         begin_btn_id:"beginBtn7_1",
         stop_btn_id:"stopBtn7_1",
         reset_btn_id: 'resetBtn7_1',
         voice_type: 0,
         time_ding_dang: [30,0]
     });
-     
+
+    $("#timedown8").custimer({  //结辩
+        time_length:240,
+        begin_btn_id:"beginBtn8",
+        stop_btn_id:"stopBtn8",
+        reset_btn_id: 'resetBtn8',
+        voice_type: 0,
+        time_ding_dang: [30,0]
+    });
+
+    $("#timedown8_1").custimer({  //结辩
+        time_length:240,
+        begin_btn_id:"beginBtn8_1",
+        stop_btn_id:"stopBtn8_1",
+        reset_btn_id: 'resetBtn8_1',
+        voice_type: 0,
+        time_ding_dang: [30,0]
+    });
+    
 
 
 
-     var matrix = [
+    var matrix = [
         [
             {full:0},{full:1,moveDirection:'yx'},{full:0}
         ],
